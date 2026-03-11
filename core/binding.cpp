87e47297bf8090319569b5ade49b93ce3b6d6696
pjh456 <147148383@qq.com>
2026-03-11 11:18:44 +0800

    feat: 初步添加了推理框架导出 API

--- core/binding.cpp
+++ core/binding.cpp
@@ -1,15 +1,208 @@
-#ifndef INCLUDE_ASTRA_RP_BINDING_CPP
-#define INCLUDE_ASTRA_RP_BINDING_CPP
-
 #include <napi.h>
+#include <thread>
+#include <memory>
+#include <vector>
 
 #include "core/global_config.hpp"
+#include "pipeline/graph.hpp"
+#include "pipeline/scheduler.hpp"
+#include "pipeline/event_bus.hpp"
+#include "pipeline/format_node.hpp"
+#include "pipeline/inference_node.hpp"
 
-Napi::Object Init(Napi::Env env, Napi::Object exports)
+using namespace astra_rp;
+
+// 1. 暴露全局初始化方法
+Napi::Value InitSystem(const Napi::CallbackInfo &info)
 {
-    return exports;
+    Napi::Env env = info.Env();
+    if (info.Length() < 1 || !info[0].IsString())
+    {
+        Napi::TypeError::New(env, "String expected for config path").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    std::string configPath = info[0].As<Napi::String>().Utf8Value();
+    auto res = core::GlobalConfigManager::instance().load_from_file(configPath);
+
+    if (res.is_err())
+    {
+        Napi::Error::New(env, res.unwrap_err().to_string()).ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    return Napi::Boolean::New(env, true);
 }
 
-NODE_API_MODULE(astrarp_node, Init)
+// 2. 异步 Worker：用于在后台运行 Scheduler，不阻塞 Node 主线程
+class PipelineRunWorker : public Napi::AsyncWorker
+{
+private:
+    std::shared_ptr<pipeline::Scheduler> m_scheduler;
+    utils::Result<void, utils::Error>
+        m_result =
+            utils::Result<void, utils::Error>::Ok();
 
-#endif // INCLUDE_ASTRA_RP_BINDING_CPP
+public:
+    PipelineRunWorker(Napi::Function &callback, std::shared_ptr<pipeline::Scheduler> scheduler)
+        : Napi::AsyncWorker(callback), m_scheduler(scheduler) {}
+
+    ~PipelineRunWorker() {}
+
+    // 在 C++ 后台线程中执行
+    void Execute() override
+    {
+        m_result = m_scheduler->run();
+    }
+
+    // 在 Node 主线程中回调
+    void OnOK() override
+    {
+        Napi::HandleScope scope(Env());
+        if (m_result.is_err())
+        {
+            Callback().Call({Napi::Error::New(Env(), m_result.unwrap_err().to_string()).Value()});
+        }
+        else
+        {
+            Callback().Call({Env().Null(), Napi::String::New(Env(), "Success")});
+        }
+    }
+};
+
+// 3. 包装 Pipeline/DAG 供 JS 调用
+class PipelineWrapper : public Napi::ObjectWrap<PipelineWrapper>
+{
+private:
+    std::shared_ptr<pipeline::Graph> m_graph;
+    std::shared_ptr<pipeline::EventBus> m_bus;
+
+    // 保存 ThreadSafeFunction 以防止被垃圾回收
+    Napi::ThreadSafeFunction m_tsfn_token;
+
+public:
+    static Napi::Object Init(Napi::Env env, Napi::Object exports)
+    {
+        Napi::Function func = DefineClass(env, "Pipeline", {InstanceMethod("addFormatNode", &PipelineWrapper::AddFormatNode), InstanceMethod("addInferenceNode", &PipelineWrapper::AddInferenceNode), InstanceMethod("addEdge", &PipelineWrapper::AddEdge), InstanceMethod("onToken", &PipelineWrapper::OnToken), InstanceMethod("run", &PipelineWrapper::Run)});
+
+        Napi::FunctionReference *constructor = new Napi::FunctionReference();
+        *constructor = Napi::Persistent(func);
+        env.SetInstanceData(constructor);
+
+        exports.Set("Pipeline", func);
+        return exports;
+    }
+
+    PipelineWrapper(const Napi::CallbackInfo &info) : Napi::ObjectWrap<PipelineWrapper>(info)
+    {
+        m_graph = std::make_shared<pipeline::Graph>();
+        m_bus = std::make_shared<pipeline::EventBus>();
+    }
+
+    // 添加格式化节点 (简化演示，实际可传入 JS 回调作为 Formatter)
+    Napi::Value AddFormatNode(const Napi::CallbackInfo &info)
+    {
+        Napi::Env env = info.Env();
+        std::string id = info[0].As<Napi::String>().Utf8Value();
+        std::string prompt = info[1].As<Napi::String>().Utf8Value(); // 简单的静态 prompt 演示
+
+        auto formatter = [prompt](const HashMap<Str, pipeline::NodePayload> &inputs) -> Str
+        {
+            return prompt; // 实际逻辑可以合并 inputs
+        };
+
+        auto node = std::make_shared<pipeline::FormatNode>(id, m_bus, formatter);
+        m_graph->add_node(node);
+
+        return env.Undefined();
+    }
+
+    // 添加推理节点
+    Napi::Value AddInferenceNode(const Napi::CallbackInfo &info)
+    {
+        Napi::Env env = info.Env();
+        std::string id = info[0].As<Napi::String>().Utf8Value();
+
+        // 使用你框架中的默认构造
+        infer::TokenizeParams tp;
+        infer::DecodeParams dp;
+        infer::SampleParams sp;
+
+        auto nodeRes = pipeline::InferenceNode::default_create(id, tp, dp, sp, m_bus);
+        if (nodeRes.is_err())
+        {
+            Napi::Error::New(env, nodeRes.unwrap_err().to_string()).ThrowAsJavaScriptException();
+            return env.Undefined();
+        }
+
+        auto node = std::make_shared<pipeline::InferenceNode>(nodeRes.unwrap());
+        m_graph->add_node(node);
+
+        return env.Undefined();
+    }
+
+    // 添加边
+    Napi::Value AddEdge(const Napi::CallbackInfo &info)
+    {
+        std::string from = info[0].As<Napi::String>().Utf8Value();
+        std::string to = info[1].As<Napi::String>().Utf8Value();
+        m_graph->add_edge(from, to);
+        return info.Env().Undefined();
+    }
+
+    // 绑定 Token 流式输出回调
+    Napi::Value OnToken(const Napi::CallbackInfo &info)
+    {
+        Napi::Env env = info.Env();
+        Napi::Function jsCallback = info[0].As<Napi::Function>();
+
+        // 创建线程安全的函数，因为 Scheduler 的 run 是在 C++ 子线程中调用回调的
+        m_tsfn_token = Napi::ThreadSafeFunction::New(
+            env, jsCallback, "TokenCallback", 0, 1);
+
+        // 订阅 C++ 的 EventBus
+        m_bus->subscribe_token([this](const Str &id, const Str &text)
+                               {
+            // 包装参数传给 JS
+            auto callback =[](Napi::Env env, Napi::Function jsCb, std::pair<Str, Str>* data) {
+                jsCb.Call({ Napi::String::New(env, data->first), Napi::String::New(env, data->second) });
+                delete data;
+            };
+            // 将数据发往 Node 主线程执行
+            auto* data = new std::pair<Str, Str>(id, text);
+            m_tsfn_token.BlockingCall(data, callback); });
+
+        return env.Undefined();
+    }
+
+    // 执行流水线
+    Napi::Value Run(const Napi::CallbackInfo &info)
+    {
+        Napi::Env env = info.Env();
+        Napi::Function jsCallback = info[0].As<Napi::Function>();
+
+        // 验证图
+        auto validRes = m_graph->validate();
+        if (validRes.is_err())
+        {
+            Napi::Error::New(env, validRes.unwrap_err().to_string()).ThrowAsJavaScriptException();
+            return env.Undefined();
+        }
+
+        // 创建 Scheduler 并通过 AsyncWorker 在后台执行
+        auto scheduler = std::make_shared<pipeline::Scheduler>(m_graph, 4, m_bus);
+        PipelineRunWorker *worker = new PipelineRunWorker(jsCallback, scheduler);
+        worker->Queue();
+
+        return env.Undefined();
+    }
+};
+
+// 4. 导出模块
+Napi::Object InitAll(Napi::Env env, Napi::Object exports)
+{
+    exports.Set("initSystem", Napi::Function::New(env, InitSystem));
+    return PipelineWrapper::Init(env, exports);
+}
+
+NODE_API_MODULE(astra_rp, InitAll)
