c850d79ad6d331f23e5ad3f17c8d5b023ac558a0
pjh456 <147148383@qq.com>
2026-03-26 18:50:39 +0800

    test: 添加图断连节点用例

---
+++ core/tests/test_graph_disconnected.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#include "pipeline/graph.hpp"
+#include "pipeline/base_node.hpp"
+
+namespace astra_rp
+{
+    namespace pipeline
+    {
+        class DummyNode final : public BaseNode
+        {
+        public:
+            DummyNode(const Str &id, MulPtr<EventBus> bus = nullptr)
+                : BaseNode(id, bus) {}
+
+            ResultV<void> execute() override
+            {
+                return ResultV<void>::Ok();
+            }
+        };
+    }
+}
+
+int main()
+{
+    using namespace astra_rp;
+
+    pipeline::Graph graph;
+    graph.add_node(std::make_shared<pipeline::DummyNode>("A"));
+    graph.add_node(std::make_shared<pipeline::DummyNode>("B"));
+
+    auto res = graph.validate();
+    if (res.is_err())
+    {
+        std::cerr << "Expected disconnected graph to be valid, got error: "
+                  << res.unwrap_err().to_string() << std::endl;
+        return 1;
+    }
+
+    std::cout << "Graph disconnected nodes test passed." << std::endl;
+    return 0;
+}
